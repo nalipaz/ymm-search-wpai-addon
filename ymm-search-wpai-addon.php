a6a3d4412eb557423e3ae1fbcb0f32e48c5b0772
Nicholas Alipaz <nicholas@alipaz.net>
2022-12-06 04:35:35 -0800

    Fix plugin name

--- ymm-search-wpai-addon.php
+++ ymm-search-wpai-addon.php
@@ -1,9 +1,9 @@
 <?php
 /*
-Plugin Name: WP All Import Yoast SEO Add-On
-Description: A complete example add-on for importing data to certain Yoast SEO fields.
+Plugin Name: WP All Import YMM Search Add-On
+Description: Imports configured template data into the tables for the YMM Search Plug-in (Year, Make, and Model).
 Version: 1.0
-Author: WP All Import
+Author: Nicholas Alipaz
 */
 
 include "rapid-addon.php";
@@ -54,7 +54,7 @@ final class YMM_Search_Add_On {
                 );
             }
             
-            // Only run this add-on if the free or pro version of the Yoast plugin is active.
+            // Only run this add-on if the YMM Search plugin is active.
             if (is_plugin_active('ymm-search/ymm-search.php')) {
                 $this->add_on->run();
             }
@@ -68,13 +68,13 @@ final class YMM_Search_Add_On {
     // Check if the user has allowed these fields to be updated, and then import data to them
     public function import($post_id, $data, $import_options) {
         if ($this->add_on->can_update_meta('_ymm_search_restrictions', $import_options)) {
-            
             if (isset($data['ymm_search_restrictions'])) {
                 $restriction = sanitize_textarea_field(stripslashes($data['ymm_search_restrictions']));
 
                 try {
                     $this->ymm_db->saveProductRestrictionText($post_id, $restriction);
-                } catch (Exception $e) {
+                }
+                catch (Exception $e) {
                     $logger = function($m) { printf("[%s] $m", date("H:i:s")); flush(); };
                     call_user_func($logger, 'YMM restriction was not saved.' . ' ' . $this->getFormatExplanationMessage()); 
                 }
